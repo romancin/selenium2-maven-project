3456519ed65e18490e1c69e4c238182c351119f3
Román Martín <roman.martin84@gmail.com>
2021-10-22 10:07:54 +0200

    Update GoogleStartPageTest.java

--- src/test/java/de/mb/selenium/GoogleStartPageTest.java
+++ src/test/java/de/mb/selenium/GoogleStartPageTest.java
@@ -28,7 +28,7 @@ public class GoogleStartPageTest {
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		URL gridUrl = new URL(gridServerUrl);
 		driver = new RemoteWebDriver(gridUrl, capability);
-		driver.get("https://www.bricomart.es/");
+		driver.get("https://www.paradigmadigital.com/");
 	}
 
 	@After
