a85c3a9087484b7c8495f50e763ca98b9b6f0b68
Román Martín <roman.martin84@gmail.com>
2021-10-22 11:04:41 +0200

    Update GoogleStartPageTest.java

--- src/test/java/de/mb/selenium/GoogleStartPageTest.java
+++ src/test/java/de/mb/selenium/GoogleStartPageTest.java
@@ -12,6 +12,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
